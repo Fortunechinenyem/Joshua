8c82f8fa165577a73d338857439e69e3ca640fc4
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-02 20:54:13 +0100

    initial setup

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.className";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Joshua Aribido",
+  description: "Entrepreneur, Auto- Consultant, Certified CNG Installer",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
